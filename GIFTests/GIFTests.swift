7f2334e93bc205fefa07d22bf0f1fcd5ade44090
Nick Lee <nick@nickplee.com>
2014-09-04 19:41:45 -0400

    Initial Commit

---
+++ GIFTests/GIFTests.swift
@@ -0,0 +1,36 @@
+//
+//  GIFTests.swift
+//  GIFTests
+//
+//  Created by Nick Lee on 9/4/14.
+//  Copyright (c) 2014 Nicholas Lee Designs, LLC. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class GIFTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
